c596c1d97a3163ff858e027f01156601d5ab84d0
asinoai <aron.sinoai@linkonomics.com>
2016-05-19 14:33:57 +0300

    Renaming Main to Application

--- console/src/main/java/aron/sinoai/consolespringsample/spring/Application.java
+++ console/src/main/java/aron/sinoai/consolespringsample/spring/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /**
  */
-public class Main {
+public class Application {
     public static void main(String[] args) {
 
         final ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
